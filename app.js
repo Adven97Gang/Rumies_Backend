4c2f66a50854d7d56a6bc72fe8c20e8fdcab9bff
Adven97 <atomczak30@gmail.com>
2019-10-19 13:39:41 +0200

    test now xd"

--- app.js
+++ app.js
@@ -1,25 +1,13 @@
 const express = require('express');
 const app = express();
-const mongoose = require('mongoose');
 require('dotenv/config');
-const postRoute = require('./routes/posts');
-const bodyParser = require('body-parser');
-const cors = require('cors');
 
-const url = process.env.DB_CONNECTION;
 const port = process.env.PORT;
 
-app.use(cors());
-app.use(bodyParser.json());
-app.use('/posts', postRoute)
 
 app.get('/', (req, res) => {
     res.send('WELCOME RUMIES DEV TEAM :D')
 })
 
-mongoose.connect(url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}, () => console.log('connected to DB! :D'));
 
 app.listen(port);
