09cbcd16466df3ee0ea0e1608a3c1d0df081017a
Adven97 <atomczak30@gmail.com>
2019-10-28 22:37:44 +0100

    add model for Users and endpoint for register

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 require('dotenv/config');
+
 const postRoute = require('./routes/posts');
+const userRoute = require('./routes/users');
+
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
@@ -12,10 +15,11 @@ const port = process.env.PORT;
 app.use(cors());
 app.use(bodyParser.json());
 
-app.use('/posts', postRoute)
+app.use('/posts', postRoute);
+app.use('/users', userRoute);
 
 app.get('/', (req, res) => {
-    res.send('WELCOME RUMIES DEV TEAM :D v 2.0')
+    res.send('WELCOME RUMIES DEV TEAM :D v 2.1')
 })
 
 mongoose.connect(url, {
